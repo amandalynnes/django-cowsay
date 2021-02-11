0e26625dc66a8c42167d92e52bd9cb71c5ae24e5
Amanda Simmons <carpe737diem@gmail.com>
2021-02-11 18:57:32 -0500

    worked with Cesar to figure out why my caowsay was not working, moved subprocess inside the post request, and accessed the message to render via dot notation, also converted cow from byte string so that it looked correct

--- cowsay_app/views.py
+++ cowsay_app/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 
 from cowsay_app.models import TextItem
 from cowsay_app.forms import AddTextForm
+import subprocess
 # Create your views here.
 
-
+# Cesar Ramos helped me get my cow to show up. I was close but needed to tweak some things.
 def index_view(request):
     form = AddTextForm()
 
@@ -13,10 +14,16 @@ def index_view(request):
         form = AddTextForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            new_text = TextItem.objects.create(
+            TextItem.objects.create(
                 text=data['text'],
             )
-            return HttpResponseRedirect(reverse('homepage'))
+            cow_say = subprocess.run(['cowsay', data['text']], capture_output=True)
+            print(cow_say.stdout.decode('utf-8'))
+            cow = cow_say.stdout.decode('utf-8')
+            form = AddTextForm()
+            return render(request, "index.html", {
+                "heading": "Welcome to Cowsay, 'MOOOOOOO!'", 'form': form, 'cow': cow}
+            )
 
     form = AddTextForm()
     return render(request, "index.html", {
